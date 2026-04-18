7bdaebb3cd517bcf1d05e9868489d958f654d634
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-04-18 19:28:24 +0800

    fix product option cleaning

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -5,6 +5,9 @@ import { prisma } from "@/lib/prisma";
 
 async function main() {
   await prisma.canteenMap.deleteMany();
+  await prisma.productOptionValue.deleteMany();
+  await prisma.productOption.deleteMany();
+  await prisma.product.deleteMany();
   await prisma.shopCart.deleteMany();
   await prisma.shop.deleteMany();
   await prisma.user.deleteMany();
