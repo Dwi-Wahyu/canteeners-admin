b89e79c796c254c9ec2036598f751ceff957a835
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-04-18 19:29:32 +0800

    fix seeder

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -5,6 +5,10 @@ import { prisma } from "@/lib/prisma";
 
 async function main() {
   await prisma.canteenMap.deleteMany();
+  await prisma.payment.deleteMany();
+  await prisma.shopTestimony.deleteMany();
+  await prisma.shopComplaint.deleteMany();
+  await prisma.shopBilling.deleteMany();
   await prisma.productOptionValue.deleteMany();
   await prisma.productOption.deleteMany();
   await prisma.product.deleteMany();
